21e059b9faba9cbd9bd74b442dddfb5760af39ed
afarrell4 <63366168+afarrell4@users.noreply.github.com>
2020-04-15 12:01:09 -0400

    Update CQ_TRIAL.m

--- CQ_TRIAL.m
+++ CQ_TRIAL.m
@@ -9,6 +9,23 @@
 
 %%List all parameters:
 
+%Patient
+Weight = 50;  %units = kg
+
+% Dosing
+Tablet = 250; %units = mg
+DoseC  = 25;  %units - mg/kg
+Dose = 25*Weight; %units = mg
+
+
+%25mg/kg body weight chloroquine phosphate over 3 days
+%250 mg chloroquine phosphate per tablet
+%10 and 5 mg/kg at 0 and 6-12 hr at day 0**
+%6 mg/kg on each day 1 and day 2
+%Primaquine, 15 mg base per table
+%daily doses of 15mg base/kg body weight daily for 14 days
+%*starting from the second day (day 1) of chloroquine treatment
+
 %Volume of Distributions
 
 V1 = 468;     % units = L, central compartment of chloroquine
@@ -35,7 +52,7 @@ DHF = 8.74; %units = days, half-life DCQ
 
 
 %%Create parameter array
-% p = []
+p = [];
 
 %%Run Smiulation
 %options = odeset('MaxStep',5e-2, 'AbsTol', 1e-5,'RelTol', 1e-5,'InitialStep', 1e-2);
