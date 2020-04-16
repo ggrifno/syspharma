c19fa32a2b6284e9e7072c288e1bfa215a07cb64
ggrifno <43116793+ggrifno@users.noreply.github.com>
2020-04-16 12:50:56 -0500

    Update Chloroquine_eqns.m

--- Chloroquine_eqns.m
+++ Chloroquine_eqns.m
@@ -18,23 +18,22 @@ k34= p(11); %transfer of DCQ from central to peripheral
 k43= p(12); %transfer of DCQ from central to peripheral
 ka = p(13);  %absorption from gut to central compartment
 %protein binding of CQ in the peripheral compartment
-kon = p(14);
-koff= p(15);
+% kon = p(14);
+% koff= p(15);
 
 %two-compartment model of Chloroquine ORIGINAL
 %question: model drawing implies that drug is only cleared from blood, not
 %from peripheral tissues...
 dydt = zeros(8,1); %column vector contains all of the values for the 
 dydt(1) = q/V1 + ka*y(4)/V1 - k10*y(1) - k12*y(1) + (V2/V1)*k21*y(2) - k23*y(1) + koff*y(7) - kon*y(1); % CQ central compartment, q is only used in case of IV delivery, but chloroquine is always delivered orally
-dydt(2) =                   (V1/V2)*k12*y(1) - k21*y(2);              % CQ peripheral compartment
-dydt(3) =                     k10*y(1)*V1;                            % virtual clearance of CQ, only cleared from central compartment
-dydt(4) =      - ka*y(4);                                             %CQ absorption compartment (gut??) will be useful in modeling oral delivery in boluses
+dydt(2) =(V1/V2)*k12*y(1) - k21*y(2); % CQ peripheral compartment
+dydt(3) = k10*y(1)*V1;                % virtual clearance of CQ, only cleared from central compartment
+dydt(4) = - ka*y(4);                  % CQ absorption compartment (gut??) will be useful in modeling oral delivery in boluses
 dydt(5) = k23*y(1)*V1 + k43*y(6) - (k30*y(5) + k34*y(6)); % conversion of CQ to DCQ, central compartment NEED VOLUME CORRECTIONS??
 dydt(6) = k34*y(5) - (V2/V1)*k43*y(6); % DCQ peripheral comparmtent
 dydt(7) = kon*y(1); % amount of CQ that is bound to serum proteins
 dydt(8) = koff*y(7);% amount of CQ that is coming unbound from serum proteins
-dydt(9) = k10*y(1); % virtual clearance of CQ from central compartment
-dydt(10) = k30*y(5); % virtual clearance of DCQ, is only cleared from central DCQ
+dydt(10)= k30*y(5); % virtual clearance of DCQ, is only cleared from central DCQ
 
 % 1 - concentration of drug in central compartment (infusion included here, but typically set to zero in simulations of oral delivery)
 % 2 - conceentration of drug in peripheral compartment (clearance included here, but typically set to zero in simulations)
