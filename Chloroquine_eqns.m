fe54c93cf1d82420fb19963b75e6f0913bbfb3a4
ggrifno <43116793+ggrifno@users.noreply.github.com>
2020-04-16 12:12:43 -0500

    fixed repeat clearance formulas

--- Chloroquine_eqns.m
+++ Chloroquine_eqns.m
@@ -27,12 +27,11 @@ ka = p(13);  %absorption from gut to central compartment
 dydt = zeros(7,1); %column vector contains all of the values for the 
 dydt(1) = q/V1 + ka*y(4)/V1 - k10*y(1) - k12*y(1) + (V2/V1)*k21*y(2) - k23*y(1); % CQ central compartment, q is only used in case of IV delivery, but chloroquine is always delivered orally
 dydt(2) = (V1/V2)*k12*y(1) - k21*y(2); % CQ peripheral compartment
-dydt(3) = kc1*y(1)*V1;                 % virtual clearance of CQ, only cleared from central compartment
-dydt(4) = - ka*y(4);                   % GUT CQ absorption compartment --> set the initial condition of y(4) to the bolus tablet amount D0 to model oral delivery
-dydt(5) = k23*y(1)*V1 + k43*y(6) - (k30*y(5) + k34*y(6)); % conversion of CQ to DCQ, central compartment NEED VOLUME CORRECTIONS??
-dydt(6) = k34*y(5) - (V2/V1)*k43*y(6); % DCQ peripheral comparmtent
-dydt(7) = k10*y(1);                    % virtual clearance of CQ from central compartment
-dydt(8 )= k30*y(5);                    % virtual clearance of DCQ, is only cleared from central DCQ
+dydt(3) = -ka*y(4); % GUT CQ absorption compartment --> set the initial condition of y(4) to the bolus tablet amount D0 to model oral delivery
+dydt(4) = k23*y(1)*V1 + k43*y(6) - (k30*y(5) + k34*y(6)); % conversion of CQ to DCQ, central compartment NEED VOLUME CORRECTIONS??
+dydt(5) = k34*y(5) - (V2/V1)*k43*y(6); % DCQ peripheral comparmtent
+dydt(6) = k10*y(1)*V1; % virtual clearance of CQ from central compartment
+dydt(7) = k30*y(5)*V1; % virtual clearance of DCQ, is only cleared from central DCQ
 
 % 1 - concentration of CQ in central compartment (infusion included here, but typically set to zero in simulations of oral delivery)
 % 2 - conceentration of CQ in peripheral compartment (clearance included here, but typically set to zero in simulations)
@@ -46,3 +45,45 @@ dydt(8 )= k30*y(5);                    % virtual clearance of DCQ, is only clear
 % 7 - virtual clearance of CQ from central compartment
 % 8 - virtual clearance of DCQ from central compartment
 
+%CLEARANCE COMPARTMENTS: Note unit change
+% here, the terms in the equation have conc*vol form, which means amount 
+% not concentration. This eliminates the need to set a volume for this
+% compartment; just need to be careful in the main code to treat it like an
+% amount, not a concentration.
+
+%% COMPARISON TO ACET_EQNS
+% function dydt = Acet_eqns(t,y,p)
+% % equations describing the pharmacokinetics of acetaminophen, delivered
+% % orally, in a two-compartment model with absorption from the gut and
+% % clearance from the central compartment
+% 
+% q=p(1);
+% V1=p(2);
+% V2=p(3);
+% kc1=p(4);
+% kc2=p(5);
+% k12=p(6);
+% k21=p(7);
+% ka =p(8);
+% dydt = zeros(4,1);    % use a column vector 
+%  
+% %% EQUATIONS
+% % 1 - concentration of drug in central compartment (infusion included here, 
+% % but typically set to zero in simulations of oral delivery)
+% % 2 - conceentration of drug in peripheral compartment (clearance included 
+% % here, but typically set to zero in simulations)
+% % 3 - amount of drug in virtual clearance compartment. Note unit change
+% % here, the terms in the equation have conc*vol form, which means amount 
+% % not concentration. This eliminates the need to set a volume for this
+% % compartment; just need to be careful in the main code to treat it like an
+% % amount, not a concentration.
+% % 4 - amount of drug in virtual gut compartment. Again, the units here are
+% % in amount, not concentration. Note how y4 is divided by V in equation 1
+% 
+%  dydt(1) = q/V1 + ka*y(4)/V1 - kc1*y(1) - k12*y(1) + (V2/V1)*k21*y(2);
+%  dydt(2) =                   - kc2*y(2) + (V1/V2)*k12*y(1) - k21*y(2);
+%  dydt(3) =                     kc1*y(1)*V1 + kc2*y(2)*V2;
+%  dydt(4) =      - ka*y(4);
+%  
+%  
+% 
