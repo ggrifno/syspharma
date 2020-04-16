f3c7ba24aaa01c6c01aa1c6777aef031fff7f8f4
afarrell4 <63366168+afarrell4@users.noreply.github.com>
2020-04-16 12:25:44 -0400

    Update

--- CQ_TRIAL.m
+++ CQ_TRIAL.m
@@ -4,7 +4,7 @@
 %Layout: 
 %(1) declare necessary parameters/dosing/timeframe/patient weight
 %(2) initiate the simulation (*include mass balance)
-*(3) plot concentration across different compartments + mass balance
+%(3) plot concentration across different compartments + mass balance
 
 
 %%List all parameters:
@@ -18,6 +18,8 @@ DoseC  = 25;  %units - mg/kg
 Dose = 25*Weight; %units = mg
 NumberOfDoses = 3;
 TimeBetweenDoses = 0;
+q = 0;
+ka = 0;
 
 
 %25mg/kg body weight chloroquine phosphate over 3 days
@@ -35,7 +37,7 @@ V1 = 468;     % units = L, central compartment of chloroquine
 V2 = 1600;    % units - L, peripheral compartment of chloroquine
 V3 = 2.27;    % units = L, central compartment of DCQ
 V4 = 566257;  % units = L, peripheral compartment of DCQ
-V5 = 0;       % is there a volume for the gut? absportion?
+V5 = 10;       % is there a volume for the gut? absportion?
 
 %clearence rates
 k12 = 37.7;   % units = L/h, Chloroquine from central to peripheral
@@ -45,6 +47,8 @@ k34 = 31.46;  % units = L/h, DCQ from central to peripheral
 k43 = 31.46;  % units = L/h, DCQ from peripheral to central
 k10 = 0;      % units, clearance of CQ
 k30 = 2.04;   % units = L/h, peripheral compartment of DCQ
+kon = 0;
+koff = 0;
 
 CHF = 10.7; %units = days, half-life CQ
 DHF = 8.74; %units = days, half-life DCQ
@@ -55,42 +59,54 @@ DHF = 8.74; %units = days, half-life DCQ
 %Set innitial concentration of the gut to the dose
 %set time framre
 
-y0 = [Dose/V5 0 0 0 0 0]
-% (1) Concentration of CQ in the gut  (2) concentration of CQ in the central compartment
-% (3) Concentration of CQ in the peripheral compartment
-% (4) Concentration of DCQ in the central compartment
-% (5) Concentration of DCQ in the peripheral compartment
-% (6) Drug Cleared
+y0 = [0 0 0 Dose/V5 0 0 0]
+% (1) Concentration of CQ in the central compartment
+% (2) Concentration of CQ in the peripheral compartment
+% (3) Amount of Drug in Virtual Clearance Compartment
+% (4) Amount of Drug in Virtual Gut Compartment
+% (5) Concentration of DCQ in the central compartment (i.e converstion of
+% CQ to DCQ)
+% (6) Concentration of DCQ in the peripheral compartment
+% (7) Drug Cleared
 
 
 %%Create parameter array
-p = [];
+p = [q V1 V2 V3 V4 k10 k30 k12 k21 k23 k34 k43 ka];
 
 %%Run Smiulation
-%options = odeset('MaxStep',5e-2, 'AbsTol', 1e-5,'RelTol', 1e-5,'InitialStep', 1e-2);
-[T1, Y1] = ode45(@Acet_eqns,[0 TimeLen],y0,options,p);
+options = odeset('MaxStep',5e-2, 'AbsTol', 1e-5,'RelTol', 1e-5,'InitialStep', 1e-2);
+[T1, Y1] = ode45(@Chloroquine_eqns,[0 24],y0,options,p);
 
-T = [T; T1];
-Y = [Y, Y1];
+% T = [T; T1];
+% Y = [Y, Y1];
+
 %update initial conditions for next simulation
 y00 = [];
-for i = 2:NumberOfDoses
-  [t,y] = ode45(@CQ_eqns,[0 TimeBetweenDoses],y00,options,p);
-  
-  %update time and concentration values for entire simulation
-  T = [T; t];
-  Y = [Y; y];
-  
-  %include mass balance
-  
-  %last concentration values become new initial conditions for next
-  %dosing simulation
-  y00 = y(end,:);
-end
+% for i = 2:NumberOfDoses
+%   [t,y] = ode45(@CQ_eqns,[0 TimeBetweenDoses],y00,options,p);
+%   
+%   %update time and concentration values for entire simulation
+%   T = [T; t];
+%   Y = [Y; y];
+%   
+%   %include mass balance
+%   
+%   %last concentration values become new initial conditions for next
+%   %dosing simulation
+%   y00 = y(end,:);
+% end
 
 %% Calculate Mass balance
 
 
 %% Plot
+
+figure; %
+
+plot(T1, Y1(:,1),'b-','linewidth',3)
+
+title('Concentration of Free Drug in Blood')
+ylabel('[D] (nM)')
+xlabel('time (hrs)')
 
 
