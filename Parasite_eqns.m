bebb42809523845cd95d21f2b7456220c59812c2
ggrifno <43116793+ggrifno@users.noreply.github.com>
2020-04-25 22:31:19 -0500

    deleted old comments in parasite eqns

--- Parasite_eqns.m
+++ Parasite_eqns.m
@@ -6,10 +6,7 @@ kP = a(1);% kP is the clearance rate of parasites
 r = a(2);% r = growth rate of parasites, assuming a doubling rate of 24 hrs
 
 %P is the current 
-dydt = zeros(3,1);
-% dydt(1) = P*exp(-kP*t)*(-kP) -P*exp(r*t)*r ; %parasite balance, total # of parasites in blood
-% dydt(2) = P*exp(-kP*t)*(-kP) ;  % parasite clearance only
-% dydt(3) = -P*exp(r*t)*r;        % parasite growth only
+dydt = zeros(3,1); %initialize the dydt object as a column to avoid throwing errors in ode45
 dydt(1) = r*P(1) - kP*P(1);%parasite balance, total # of parasites in blood
 dydt(2) = -kP*P(1) ;  % parasite clearance only
 dydt(3) = r*P(1);    % parasite growth only
