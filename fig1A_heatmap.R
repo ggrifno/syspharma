c318e305ffa5718c38fac30339dd8d269f9ce8ac
ggrifno <43116793+ggrifno@users.noreply.github.com>
2020-04-27 20:04:37 -0500

    made first heatmaps of efficacy data

--- fig1A_heatmap.R
+++ fig1A_heatmap.R
@@ -9,30 +9,52 @@ library(ggplot2)
 library(ggpubr)
 library(tidyverse)
 library(reshape2) #need this to use melt funtion
+library(plotly)
 #8 hours --------------------------------------------------
 
 #get the simulation data from MATLAB
 Data <- readMat('figA1.mat',header=T)
 df <- as.data.frame(Data)# Convert from list to data frame
+# df_values <- select(df, 'percentCured.1','percentCured.2', 'percentCured.3', 'percentCured.4', 'percentCured.5', 'percentCured.6','percentCured.7', 'percentCured.8', 'percentCured.9', 'percentCured.10')
+df_values <- select(df, 'percentCured.10','percentCured.9', 'percentCured.8', 'percentCured.7', 'percentCured.6', 'percentCured.5','percentCured.4', 'percentCured.3', 'percentCured.2', 'percentCured.1')
 #rename the column headers from default names that were passed in from MATLAB to patients
 
-
-# names(df1)[1] <- 'Patient 1'
-# names(df1)[2] <- 'Patient 2'
-# names(df1)[3] <- 'Patient 3'
-# names(df1)[4] <- 'Patient 4'
-# names(df1)[5] <- 'Patient 5'
-# #create an extra column to hold the names of the variables across each row
+# names(df_values)[1] <- '0.040'
+# names(df_values)[2] <- '0.036'
+# names(df_values)[3] <- '0.033'
+# names(df_values)[4] <- '0.029'
+# names(df_values)[5] <- '0.026'
+# names(df_values)[6] <- '0.023'
+# names(df_values)[7] <- '0.019'
+# names(df_values)[8] <- '0.016'
+# names(df_values)[9] <- '0.013'
+# names(df_values)[10]<- '0.009'
+names(df_values)[1] <- '0.009'
+names(df_values)[2] <- '0.013'
+names(df_values)[3] <- '0.016'
+names(df_values)[4] <- '0.019'
+names(df_values)[5] <- '0.023'
+names(df_values)[6] <- '0.026'
+names(df_values)[7] <- '0.029'
+names(df_values)[8] <- '0.033'
+names(df_values)[9] <- '0.036'
+names(df_values)[10]<- '0.040'
+#create an extra column to hold the names of the variables across each row
 # df1$Outputs <- c('Dose','ka','kcl','Vd')
+df_values$Dose <- c('30.0', '33.3', '36.6', '40.0', '43.3', '46.7', '50.0', '53.3', '56.7', '60.0')
 # 
 # #melt the dataframe to put it in a format that can be used to make a heatmap
-# df1_melt <- melt(df1,id.vars='Outputs')
-# 
-# p8hr <- ggplot(df1_melt, aes(x=Outputs,y=variable,fill=value)) + geom_tile(color='black') +
-#   xlab('') + ylab('') + theme_minimal() +
-#   scale_fill_gradient2(low='darkblue',high='darkred',mid='white',midpoint=0,limit=c(-0.1,0.1),name='Sensitivity')+
-#   theme(axis.text.x =element_text(angle=90,vjust=1,hjust=1), text=element_text(size=18))+
-#   coord_fixed()+
-#   ggtitle('Q3: Local Sensitivity for AUC (8 hr)') # for the main title
-# print(p8hr)
+df_melt <- melt(df_values,id.vars='Dose')
+names(df_melt)[2]<-
+
+p <- ggplot(df_melt, aes(x=variable,y=Dose,fill=value)) + geom_tile(color='black') +
+  xlab('') + ylab('') + theme_minimal() +
+  scale_fill_gradient2(low='darkblue',high='darkred',mid='white',midpoint=80,limit=c(70,100),name='% Cured')+
+  theme(axis.text.x =element_text(angle=90,vjust=1,hjust=1), text=element_text(size=18))+
+  coord_fixed()+
+  xlab('Parasite Clearance Rate, kP (hr-1)')+
+  ylab('Dose (mg/kg)')+
+  ggtitle('Figure 1A: Efficacy Across Variable CQ Dose and kP') # for the main title
+print(p)
+ggplotly(p)
 # ggsave(file="Q3_8hr.png",plot=p8hr,width=12,height=6)
