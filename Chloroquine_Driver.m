0bb4052e483714d9908a4a2b4150de5a1f7f352a
ggrifno <43116793+ggrifno@users.noreply.github.com>
2020-04-27 21:35:39 -0500

    finalized figure 1 for PD

--- Chloroquine_Driver.m
+++ Chloroquine_Driver.m
@@ -213,21 +213,36 @@ end
 %approach: 
 %1. get timestamps for when concentration of CQ falls below the MIC
 %(original MIC)
-CQconcBelow = zeros(doseRange, patients); %contains the indicies in each timecourse, for each patient, for each dosage, where [CQ] drops below MIC  
+CQconcBelow_MIC1 = zeros(doseRange, patients); %contains the indicies in each timecourse, for each patient, for each dosage, where [CQ] drops below MIC  
 Hour_100 = 1672; %the index of the time vector that is approximately 100 hours into the 500 total hour simulation
 for dose = 1:doseRange
     for patient = 1: patients
         sample = CQconc(Hour_100:end,patient,dose); %select the patient's concentration timecourse for each dosage, after about the first 100 hours (after time index is 1672)
         if size(find(sample < MIC1, 1)) == [0,1] %"find" will return an empty vector sometimes which cannot be added to CQconcBelow
-        CQconcBelow(dose,patient) = 0; %add a zero here as a placeholder, this will mean that the patient never dropped below MIC1
+        CQconcBelow_MIC1(dose,patient) = 0; %add a zero here as a placeholder, this will mean that the patient never dropped below MIC1
         else
-        CQconcBelow(dose,patient) = Hour_100 + find(sample < MIC1, 1); 
+        CQconcBelow_MIC1(dose,patient) = Hour_100 + find(sample < MIC1, 1); 
         %collect the index of the FIRST location in the patient's total timecourse where conentration drops below MIC, AFTER the first 50 hours when dose fluxates wildly
         end
     end
 end
 
-%% 2. break the parasite dynamics simulation into two parts, above MIC and %below MIC. 
+%(doubled MIC)
+CQconcBelow_MIC2 = zeros(doseRange, patients); %contains the indicies in each timecourse, for each patient, for each dosage, where [CQ] drops below MIC  
+Hour_100 = 1672; %the index of the time vector that is approximately 100 hours into the 500 total hour simulation
+for dose = 1:doseRange
+    for patient = 1: patients
+        sample = CQconc(Hour_100:end,patient,dose); %select the patient's concentration timecourse for each dosage, after about the first 100 hours (after time index is 1672)
+        if size(find(sample < MIC2, 1)) == [0,1] %"find" will return an empty vector sometimes which cannot be added to CQconcBelow
+        CQconcBelow_MIC2(dose,patient) = 0; %add a zero here as a placeholder, this will mean that the patient never dropped below MIC1
+        else
+        CQconcBelow_MIC2(dose,patient) = Hour_100 + find(sample < MIC2, 1); 
+        %collect the index of the FIRST location in the patient's total timecourse where conentration drops below MIC, AFTER the first 50 hours when dose fluxates wildly
+        end
+    end
+end
+
+%% 2.1 MIC1: break the parasite dynamics simulation into two parts, above MIC and %below MIC. 
 %for below MIC, set the kP to one order of manitude lower than the
 %patient's original kP, which we pick from a range for each of the patients
 %(not dependent on popPK information or bodyweight)
@@ -246,7 +261,7 @@ Parasites_at_end = ones(rangekP, doseRange,patients); %hold the final parasite n
 for clearanceCounter = 1:rangekP
 for dose = 1:doseRange %iterate through all the CQ dose settings (ten total)
 for patient =1:patients %iterate through all 100 patients
-    if CQconcBelow(dose,patient) ==0 %just looking at all 100 patients for the first dose condition (dose conditions 1/10)
+    if CQconcBelow_MIC1(dose,patient) ==0 %just looking at all 100 patients for the first dose condition (dose conditions 1/10)
     tspan = 0:.5:500; %if the patient never drops below MIC, simulate ALL observation time (500 hours)
     kP_val = kP(clearanceCounter); %run model using one of the kPs to indicate parasite sensitivity to CQ
     r = 0.009625;   %hr-1; let the growth rate of the parasite be double every 3 days = .693/72hr
@@ -260,7 +275,7 @@ for patient =1:patients %iterate through all 100 patients
     
     else %this means that the patient DOES drop below MIC
     %first half of simulation
-    droppoint = CQconcBelow(dose,patient); %time point at which the patient's [CQ] drops below MIC
+    droppoint = CQconcBelow_MIC1(dose,patient); %time point at which the patient's [CQ] drops below MIC
     tspan1 = 0:0.5:Time(droppoint,1); %index the Time vector get the TIME (in hrs) where the conc falls below 0 
     kP_val = kP(clearanceCounter); %demonstrate model using median kP AT FIRST
     r = 0.009625;   %hr-1; let the growth rate of the parasite be double every 3 days = .693/72hr
@@ -270,7 +285,8 @@ for patient =1:patients %iterate through all 100 patients
     
     %second half of simulation
     %new tspan1 with new initial conditions and new (lower) kP
-    tspan2 = tspan1(end):.06:Time(end)+10; %new tspan goes from end of first tspan to end of simulation PLUS an additional 10 hours (520 total hours, 10 hours longer than CQ simulation, which goes to 510 hours)
+%     tspan2 = tspan1(end):.06:Time(end)+10; %new tspan goes from end of first tspan to end of simulation PLUS an additional 10 hours (520 total hours, 10 hours longer than CQ simulation, which goes to 510 hours)
+    tspan2 = tspan1(end):0.5:Time(end)+10; %new tspan goes from end of first tspan to end of simulation PLUS an additional 10 hours (520 total hours, 10 hours longer than CQ simulation, which goes to 510 hours)
     p00 = P1(end,:);   %get initial conditions vector from first half of simulation
     kP_val = kP_val/10;%kP for the simulation will drops by 1 order of magnitude as CQ is no longer effective in reducing parasites
     r = 0.009625;      %hr-1; let the growth rate of the parasite be double every 3 days = .693/72hr
@@ -293,7 +309,7 @@ disp('done with kP testing number:')
 disp(clearanceCounter)
 end
 
-%% 4. calculate the number of patients cured using parasite counts at end of simulation
+%% 4.1 calculate the number of patients cured using parasite counts at end of simulation
 detect_limit = 10^9; %set the detection limit high at first for proof-of-concept
 %try for just the first dose (dose 1)
 kP_Range = 10; %number of kP's we will try
@@ -306,9 +322,94 @@ for clearanceCounter = 1:rangekP
     end
 end
 
-%% 5. export data to a .mat file for visualization as a heatmap in R
+%% 5.1 export data to a .mat file for visualization as a heatmap in R
 % FirstDoseRange = linspace(15,30, doseRange); %choose a range of first doses to explore
 % SecondDoseRange = linspace(5,10, doseRange);%choose a range of second doses to explore
 totalDose = FirstDoseRange' + 3.*SecondDoseRange'; %mg/kg 
 save figA1.mat percentCured kP totalDose;
 
+%% 2.2 MIC2: break the parasite dynamics simulation into two parts, above MIC and %below MIC. 
+%for below MIC, set the kP to one order of manitude lower than the
+%patient's original kP, which we pick from a range for each of the patients
+%(not dependent on popPK information or bodyweight)
+
+% Part 1 of Simulation, regular kP
+options = odeset('MaxStep',5e-2, 'AbsTol', 1e-5,'RelTol', 1e-5,'InitialStep', 1e-2);
+
+%decide what the kP testing range is:
+rangekP = 10; %test across 10 possible kPs
+kP = linspace(1/25,1/110,rangekP)'; %hr-1, test across 10 possible kPs in the range reported in the literature
+
+%decide what the tspan is for the first half of the simulation and simulate
+%parasite counts
+Parasites_at_end2 = ones(rangekP, doseRange,patients); %hold the final parasite number for each patient, for each dosing condition
+
+for clearanceCounter = 1:rangekP
+for dose = 1:doseRange %iterate through all the CQ dose settings (ten total)
+for patient =1:patients %iterate through all 100 patients
+    if CQconcBelow_MIC2(dose,patient) ==0 %just looking at all 100 patients for the first dose condition (dose conditions 1/10)
+    tspan = 0:.5:500; %if the patient never drops below MIC, simulate ALL observation time (500 hours)
+    kP_val = kP(clearanceCounter); %run model using one of the kPs to indicate parasite sensitivity to CQ
+    r = 0.009625;   %hr-1; let the growth rate of the parasite be double every 3 days = .693/72hr
+    a = [kP_val r]; %input vector for ODE solver
+    %let the parasite burden for all be the same (10^12 starting parasites)
+    initial_P0 = [P0 0 0]; %take in the starting # of parasites, and let number in cleared and growth "compartments" be zero
+    [T, P] = ode45(@Parasite_eqns,tspan,initial_P0,options,a); %parasite diffeq model
+    
+    %collect information from this simulation into matrix for storage
+    Parasites_at_end2(clearanceCounter,dose,patient) = P(end,1); %only grab the value from the first column
+    
+    else %this means that the patient DOES drop below MIC
+    %first half of simulation
+    droppoint = CQconcBelow_MIC2(dose,patient); %time point at which the patient's [CQ] drops below MIC
+    tspan1 = 0:0.5:Time(droppoint,1); %index the Time vector get the TIME (in hrs) where the conc falls below 0 
+    kP_val = kP(clearanceCounter); %demonstrate model using median kP AT FIRST
+    r = 0.009625;   %hr-1; let the growth rate of the parasite be double every 3 days = .693/72hr
+    a = [kP_val r]; %input vector for ODE solver
+    initial_P0 = [P0 0 0]; %take in the starting # of parasites, and let number in cleared and growth "compartments" be zero
+    [T1, P1] = ode45(@Parasite_eqns,tspan1,initial_P0,options,a); %parasite diffeq model
+    
+    %second half of simulation
+    %new tspan1 with new initial conditions and new (lower) kP
+%     tspan2 = tspan1(end):.06:Time(end)+10; %new tspan goes from end of first tspan to end of simulation PLUS an additional 10 hours (520 total hours, 10 hours longer than CQ simulation, which goes to 510 hours)
+    tspan2 = tspan1(end):0.5:Time(end)+10; %new tspan goes from end of first tspan to end of simulation PLUS an additional 10 hours (520 total hours, 10 hours longer than CQ simulation, which goes to 510 hours)
+    p00 = P1(end,:);   %get initial conditions vector from first half of simulation
+    kP_val = kP_val/10;%kP for the simulation will drops by 1 order of magnitude as CQ is no longer effective in reducing parasites
+    r = 0.009625;      %hr-1; let the growth rate of the parasite be double every 3 days = .693/72hr
+    a = [kP_val r];    %input vector for ODE solver
+    [T2, P2] = ode45(@Parasite_eqns,tspan2,p00,options,a); %parasite diffeq model
+    
+    %sum up total simualation values (both Ttot and Ptot should have same dimensions
+    T_tot = [T1; T2];
+    P_tot = [P1(:,1); P2(:,1)]; %only grab the TOTAL parasites, which are stored in first colume of ODE parasite output
+    
+    %collect information into matrix for storage
+    Parasites_at_end2(clearanceCounter,dose,patient) = P_tot(end);
+    
+    end
+end
+disp('done with dose number:')
+disp(dose)
+end
+disp('done with kP testing number:')
+disp(clearanceCounter)
+end
+
+%% 4.2 calculate the number of patients cured using parasite counts at end of simulation
+detect_limit = 10^9; %set the detection limit high at first for proof-of-concept
+%try for just the first dose (dose 1)
+kP_Range = 10; %number of kP's we will try
+percentCured2 = zeros(doseRange, kP_Range); %index position 2 will eventually be filled in by kP value
+for clearanceCounter = 1:rangekP
+    for dose = 1:doseRange
+    below_limit_indexes = find(Parasites_at_end2(clearanceCounter,dose,:) < detect_limit);
+    [row,col] = size(below_limit_indexes);
+    percentCured2(dose,clearanceCounter) = 100*row/patients; %find the percentage of total patients that were cured
+    end
+end
+
+%% 5.2 export data to a .mat file for visualization as a heatmap in R
+% FirstDoseRange = linspace(15,30, doseRange); %choose a range of first doses to explore
+% SecondDoseRange = linspace(5,10, doseRange);%choose a range of second doses to explore
+totalDose = FirstDoseRange' + 3.*SecondDoseRange'; %mg/kg 
+save figA2.mat percentCured kP totalDose;
--- fig1A_heatmap.R
+++ fig1A_heatmap.R
@@ -10,51 +10,88 @@ library(ggpubr)
 library(tidyverse)
 library(reshape2) #need this to use melt funtion
 library(plotly)
-#8 hours --------------------------------------------------
+
+#MIC1 --------------------------------------------------
 
 #get the simulation data from MATLAB
-Data <- readMat('figA1.mat',header=T)
-df <- as.data.frame(Data)# Convert from list to data frame
-# df_values <- select(df, 'percentCured.1','percentCured.2', 'percentCured.3', 'percentCured.4', 'percentCured.5', 'percentCured.6','percentCured.7', 'percentCured.8', 'percentCured.9', 'percentCured.10')
-df_values <- select(df, 'percentCured.10','percentCured.9', 'percentCured.8', 'percentCured.7', 'percentCured.6', 'percentCured.5','percentCured.4', 'percentCured.3', 'percentCured.2', 'percentCured.1')
-#rename the column headers from default names that were passed in from MATLAB to patients
-
-# names(df_values)[1] <- '0.040'
-# names(df_values)[2] <- '0.036'
-# names(df_values)[3] <- '0.033'
-# names(df_values)[4] <- '0.029'
-# names(df_values)[5] <- '0.026'
-# names(df_values)[6] <- '0.023'
-# names(df_values)[7] <- '0.019'
-# names(df_values)[8] <- '0.016'
-# names(df_values)[9] <- '0.013'
-# names(df_values)[10]<- '0.009'
-names(df_values)[1] <- '0.009'
-names(df_values)[2] <- '0.013'
-names(df_values)[3] <- '0.016'
-names(df_values)[4] <- '0.019'
-names(df_values)[5] <- '0.023'
-names(df_values)[6] <- '0.026'
-names(df_values)[7] <- '0.029'
-names(df_values)[8] <- '0.033'
-names(df_values)[9] <- '0.036'
-names(df_values)[10]<- '0.040'
+Data1 <- readMat('figA1.mat',header=T)
+df1 <- as.data.frame(Data1)# Convert from list to data frame
+#make a new dataframe with just the cure values (have to select in reverse to get axes to format correctly)
+df1_values <- select(df1, 'percentCured.10','percentCured.9', 'percentCured.8', 'percentCured.7', 'percentCured.6', 'percentCured.5','percentCured.4', 'percentCured.3', 'percentCured.2', 'percentCured.1')
+#rename the column headers from default names that were passed in from MATLAB to the kP values
+names(df1_values)[1] <- '0.009'
+names(df1_values)[2] <- '0.013'
+names(df1_values)[3] <- '0.016'
+names(df1_values)[4] <- '0.019'
+names(df1_values)[5] <- '0.023'
+names(df1_values)[6] <- '0.026'
+names(df1_values)[7] <- '0.029'
+names(df1_values)[8] <- '0.033'
+names(df1_values)[9] <- '0.036'
+names(df1_values)[10]<- '0.040'
 #create an extra column to hold the names of the variables across each row
 # df1$Outputs <- c('Dose','ka','kcl','Vd')
-df_values$Dose <- c('30.0', '33.3', '36.6', '40.0', '43.3', '46.7', '50.0', '53.3', '56.7', '60.0')
+df1_values$Dose <- c('30.0', '33.3', '36.6', '40.0', '43.3', '46.7', '50.0', '53.3', '56.7', '60.0')
 # 
 # #melt the dataframe to put it in a format that can be used to make a heatmap
-df_melt <- melt(df_values,id.vars='Dose')
-names(df_melt)[2]<-
+df1_melt <- melt(df1_values,id.vars='Dose')
+names(df1_melt)[2]<- 'kP'
+names(df1_melt)[3]<- 'Cured'
 
-p <- ggplot(df_melt, aes(x=variable,y=Dose,fill=value)) + geom_tile(color='black') +
-  xlab('') + ylab('') + theme_minimal() +
+p1a <- ggplot(df1_melt, aes(x=kP,y=Dose,fill=Cured)) + geom_tile(color='black') +
+  theme_minimal() +
   scale_fill_gradient2(low='darkblue',high='darkred',mid='white',midpoint=80,limit=c(70,100),name='% Cured')+
-  theme(axis.text.x =element_text(angle=90,vjust=1,hjust=1), text=element_text(size=18))+
+  theme(axis.text.x =element_text(angle=90,vjust=1,hjust=1), text=element_text(size=16))+
   coord_fixed()+
   xlab('Parasite Clearance Rate, kP (hr-1)')+
   ylab('Dose (mg/kg)')+
-  ggtitle('Figure 1A: Efficacy Across Variable CQ Dose and kP') # for the main title
-print(p)
-ggplotly(p)
-# ggsave(file="Q3_8hr.png",plot=p8hr,width=12,height=6)
+  ggtitle('Standard MIC: Chloroquine Efficacy for Variable Dose and Parasite Drug Sensitivity') # for the main title
+print(p1a)
+# ggplotly(p)
+# ggsave(file="fig1A.png",plot=p1a,width=12,height=6)
+
+#MIC2 --------------------------------------------------
+
+#get the simulation data from MATLAB
+Data2 <- readMat('figA2.mat',header=T)
+df2 <- as.data.frame(Data2)# Convert from list to data frame
+#make a new dataframe with just the cure values (have to select in reverse to get axes to format correctly)
+df2_values <- select(df2, 'percentCured.10','percentCured.9', 'percentCured.8', 'percentCured.7', 'percentCured.6', 'percentCured.5','percentCured.4', 'percentCured.3', 'percentCured.2', 'percentCured.1')
+#rename the column headers from default names that were passed in from MATLAB to the kP values
+names(df2_values)[1] <- '0.009'
+names(df2_values)[2] <- '0.013'
+names(df2_values)[3] <- '0.016'
+names(df2_values)[4] <- '0.019'
+names(df2_values)[5] <- '0.023'
+names(df2_values)[6] <- '0.026'
+names(df2_values)[7] <- '0.029'
+names(df2_values)[8] <- '0.033'
+names(df2_values)[9] <- '0.036'
+names(df2_values)[10]<- '0.040'
+#create an extra column to hold the names of the variables across each row
+# df1$Outputs <- c('Dose','ka','kcl','Vd')
+df2_values$Dose <- c('30.0', '33.3', '36.6', '40.0', '43.3', '46.7', '50.0', '53.3', '56.7', '60.0')
+# 
+# #melt the dataframe to put it in a format that can be used to make a heatmap
+df2_melt <- melt(df2_values,id.vars='Dose')
+names(df2_melt)[2]<- 'kP'
+names(d2f_melt)[3]<- 'Cured'
+
+p2a <- ggplot(df2_melt, aes(x=kP,y=Dose,fill=Cured)) + geom_tile(color='black') +
+  xlab('') + ylab('') + theme_minimal() +
+  scale_fill_gradient2(low='darkblue',high='darkred',mid='white',midpoint=50,limit=c(0,100),name='% Cured')+
+  theme(axis.text.x =element_text(angle=90,vjust=1,hjust=1), text=element_text(size=16))+
+  coord_fixed()+
+  xlab('Parasite Clearance Rate, kP (hr-1)')+
+  ylab('Dose (mg/kg)')+
+  ggtitle('Doubled MIC: Chloroquine Efficacy for Variable Dose and Parasite Drug Sensitivity') # for the main title
+print(p2a)
+# ggplotly(p2a)
+# ggsave(file="fig1A.png",plot=p1a,width=12,height=6)
+
+#Print/save layout--------------------------------------------------
+out <- ggarrange(p1a, p2a, nrow=1, ncol=2, common.legend = TRUE, legend = 'right')
+print(out)
+# ggsave(file="Fig1Results.png",plot=out2,width=12,height=6)
+
+
