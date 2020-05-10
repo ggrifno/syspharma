d853e1a2d3a9e2c606064f13bd54f05367a60cf5
ggrifno <43116793+ggrifno@users.noreply.github.com>
2020-05-09 19:43:06 -0500

    editing visualization for malaria local sensitivity

--- Malaria_localSensitivity.R
+++ Malaria_localSensitivity.R
@@ -32,13 +32,41 @@ names(df_AUC)[4] <- 'DCQstdev'
 
 #Local sensitivity figure 1: univariate analysis bar graph
 
-figbar <- plot_ly(df_AUC, x = c("q", "vCQ1", "vCQ2", "vDCQ1", "vDCQ2", "k10", "k30","k12", "k21", "k23", "k34", "k43", "ka"), y = ~CQ, type = 'bar', name = 'CQ central')
-figbar <- figbar %>% add_trace(y = ~DCQ, name = 'CDQ central')
-figbar <- figbar %>% layout(yaxis = list(title = 'Count', size = 18, tickfont = list(size = 16)),
-                            xaxis = list(title = 'Variable', size = 18, tickfont = list(size = 16)),
-                            barmode = 'group')
+#formatting for plotly graph
+
+f1 <- list(
+  size = 22
+)
+
+xformat <- list(
+  title = "Variables",
+  titlefont = f1,
+  showticklabels = TRUE,
+  tickangle = 360,
+  tickfont = f1
+  # exponentformat = "E"
+)
+
+yformat <- list(
+  title = "Normalized Sensitivity of AUC",
+  titlefont = f1,
+  showticklabels = TRUE,
+  # tickangle = 225,
+  tickfont = f1,
+  range = c(-1,1)
+  # exponentformat = "E"
+)
+
+figbar <- plot_ly(df_AUC, x = c("q", "vCQ1", "vCQ2", "vDCQ1", "vDCQ2", "k10", "k30","k12", "k21", "k23", "k34", "k43", "ka"), y = ~CQ, type = 'bar', name = 'CQ central',
+error_y = ~list(array = CQstdev, color= '#000000'))
+figbar <- figbar %>% add_trace(y = ~DCQ, name = 'CDQ central',error_y = ~list(array = DCQstdev, color = '#000000'))
+
+# figbar <- figbar %>% layout(yaxis = list(title = list( 'Normalized Sensitivity of AUC', size = 6), tickfont = list(size = 26)),
+#                             xaxis = list(title = list('Variable', size = 26), tickfont = list(size = 26)),
+#                             barmode = 'group')
+figbar <- figbar %>% layout(xaxis = xformat, yaxis = yformat, showlegend = TRUE, legend = list(font = list(size = 20)))
 # figbar$x <- factor(figbar$x, levels = c("q", "vCQ1", "vCQ2", "vDCQ1", "vDCQ2", "k10", "k30","k12", "k21", "k23", "k34", "k43", "ka"))
-layout(xaxis = xform)
+# layout(xaxis = xform)
 figbar
 
 
