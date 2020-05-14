ed9da1228daf9120e7b4ebe18c3f5ebbc2758215
ggrifno <43116793+ggrifno@users.noreply.github.com>
2020-05-14 14:08:23 -0500

    fixing problems with dose-variable sensitivity for COVID19

--- Chloroquine_Driver.m
+++ Chloroquine_Driver.m
@@ -13,7 +13,7 @@
 % figures exactly as they are present in the report would take several hours of run time.
 %% Run simulations for different disease and dosing cases
 clear all;
-RunCase = 1;
+RunCase = 2;
 
 % 1. Malaria    Normal Dosing
 % 2. COVID-19   Normal Dosing
