f2d24872cf02feb5f052ed5a96a5ec65af84e07e
afarrell4 <63366168+afarrell4@users.noreply.github.com>
2020-05-13 20:21:24 -0400

    R IQ Ribbon Update

--- Missed_LateDosing.R
+++ Missed_LateDosing.R
@@ -19,6 +19,7 @@ Data3_CQ <- readMat("LateDose3_Malaria_PK_CQCentral.mat",header=T) # from mat fi
 D3_CQ <- as.data.frame(Data3_CQ) # Convert .mat data into a data frame.
 Data4_CQ <- readMat("LateDose4_Malaria_PK_CQCentral.mat",header=T) # from mat file
 D4_CQ <- as.data.frame(Data4_CQ) # Convert .mat data into a data frame.
+
 DataMD_DQ <- readMat("MissedDose_Malaria_PK_DQCentral.mat",header=T) # from mat file
 DMD_DQ <- as.data.frame(DataMD_DQ) # Convert .mat data into a data frame.
 Data2_DQ <- readMat("LateDose2_Malaria_PK_DQCentral.mat",header=T) # from mat file
@@ -29,11 +30,16 @@ Data4_DQ <- readMat("LateDose4_Malaria_PK_DQCentral.mat",header=T) # from mat fi
 D4_DQ <- as.data.frame(Data4_DQ) # Convert .mat data into a data frame.
 
 pMDCQ <- ggplot(DMD_CQ, aes(x = T/24, y = MedianYCQCM0)) + 
-  geom_line(aes(color = 'Normal'),alpha = 0.7, size = 1) +
+  geom_line(aes(color = 'Normal'),alpha = 1, size = 1) +
+  geom_ribbon(aes(ymin=P25YCQCM0, ymax=P75YCQCM0), fill = 'yellow', alpha=0.1) +
   geom_line(aes(y = MedianYCQCM1, color = 'Missed First'),alpha = 0.7, size = 1) +
+  geom_ribbon(aes(ymin=P25YCQCM1, ymax=P75YCQCM1), fill = 'grey',alpha=0.3) +
   geom_line(aes(y = MedianYCQCM2, color = 'Missed Second'),alpha = 0.7, size = 1) +
-  geom_line(aes(y = MedianYCQCM3, color = 'Missed Third'),alpha = 0.7, size = 1) +
+  geom_ribbon(aes(ymin=P25YCQCM2, ymax=P75YCQCM2), fill = 'violetred4',alpha=0.1) +
+  geom_line(aes(y = MedianYCQCM3, color = 'Missed Third', ),alpha = 0.7, size = 1) +
+  geom_ribbon(aes(ymin=P25YCQCM3, ymax=P75YCQCM3), fill = 'orange',alpha=0.1) +
   geom_line(aes(y = MedianYCQCM4, color = 'Missed Fourth'),alpha = 0.7, size = 1) +
+  geom_ribbon(aes(ymin=P25YCQCM4, ymax=P75YCQCM4), fill = 'purple4', alpha=0.1) +
   #formating lines
   ggtitle('Chloroquine') + # for the main title
   xlab('Time (days)') + # for the x axis label
@@ -48,15 +54,23 @@ pMDCQ <- ggplot(DMD_CQ, aes(x = T/24, y = MedianYCQCM0)) +
         legend.title = element_blank(),
         legend.position = "none",
         plot.title = element_text(size = 11, face = "bold"),
-        axis.title = element_text(size = 10))
+        axis.title = element_text(size = 10))+
+  scale_y_continuous(limits = c(0.0, 1.2))
+  
 
 #print(pMDCQ)
 pMDDQ <- ggplot(DMD_DQ, aes(x = T/24, y = MedianYDQCM0)) + 
   geom_line(aes(color = 'Normal'),alpha = 0.7, size = 1) +
+  geom_ribbon(aes(ymin=P25YDQCM0, ymax=P75YDQCM0), fill = 'yellow', alpha=0.1) +
   geom_line(aes(y = MedianYDQCM1, color = 'Missed First'),alpha = 0.7, size = 1) +
+  geom_ribbon(aes(ymin=P25YDQCM0, ymax=P75YDQCM0), fill = 'grey', alpha=0.3) +
   geom_line(aes(y = MedianYDQCM2, color = 'Missed Second'),alpha = 0.7, size = 1) +
+  geom_ribbon(aes(ymin=P25YDQCM2, ymax=P75YDQCM2), fill = 'violetred4',alpha=0.1) +
   geom_line(aes(y = MedianYDQCM3, color = 'Missed Third'),alpha = 0.7, size = 1) +
+  geom_ribbon(aes(ymin=P25YDQCM2, ymax=P75YDQCM2), fill = 'orange',alpha=0.1) +
   geom_line(aes(y = MedianYDQCM4, color = 'Missed Fourth'),alpha = 0.7, size = 1) +
+  geom_ribbon(aes(ymin=P25YDQCM2, ymax=P75YDQCM2), fill = 'purple4',alpha=0.1) +
+  
   #formating lines
   ggtitle('Desethylchloroquine') + # for the main title
   xlab('Time (days)') + # for the x axis label
@@ -70,7 +84,9 @@ pMDDQ <- ggplot(DMD_DQ, aes(x = T/24, y = MedianYDQCM0)) +
         panel.background = element_blank(),
         legend.title = element_blank(),
         plot.title = element_text(size = 11, face = "bold"),
-        axis.title = element_text(size = 10))
+        axis.title = element_text(size = 10))+
+  scale_y_continuous(limits = c(0.0, 1.2))
+  
 
 out <- grid.arrange(pMDCQ,pMDDQ,ncol=2) 
 
@@ -81,6 +97,7 @@ pL2CQ <- ggplot(D2_CQ, aes(x = T/24, y = MedianYCQCM0)) +
   geom_line(aes(y = MedianYCQCM3, color = 'Missed Third'),alpha = 0.7, size = 1) +
   geom_line(aes(y = MedianYCQCM4, color = 'Missed Fourth'),alpha = 0.7, size = 1) +
   geom_line(aes(y = MedianYCQCM5, color = '19.2 Hours Late'),alpha = 0.7, size = 1) +
+
   #formating lines
   ggtitle('Chloroquine') + # for the main title
   xlab('Time (days)') + # for the x axis label
