12cbf024021316ec12eba8f3a6ab96bf6d562880
afarrell4 <63366168+afarrell4@users.noreply.github.com>
2020-05-14 16:22:58 -0400

    Updates

--- Malaria_v_COVID/app.R
+++ Malaria_v_COVID/app.R
@@ -66,6 +66,14 @@ ui <- fluidPage(
                         plotlyOutput('Late3DQ',height=350,width=400),
                         h5('Panel F: Chloroquine Concentration as a result of missing a dose.')
                  ),
+                 column(6,
+                        plotlyOutput('Late4CQ',height=350,width=400),
+                        h5('Panel G: Chloroquine Concentration as a result of missing a dose.')
+                 ),
+                 column(6,
+                        plotlyOutput('Late4DQ',height=350,width=400),
+                        h5('Panel H: Chloroquine Concentration as a result of missing a dose.')
+                 ),
              )))
     
   
@@ -120,8 +128,8 @@ Data2_CQ <- readMat("data/LateDose2_Malaria_PK_CQCentral.mat",header=T) # from m
 D2_CQ <- as.data.frame(Data2_CQ) # Convert .mat data into a data frame.
 Data3_CQ <- readMat("data/LateDose3_Malaria_PK_CQCentral.mat",header=T) # from mat file
 D3_CQ <- as.data.frame(Data3_CQ) # Convert .mat data into a data frame.
-#Data4_CQ <- readMat("LateDose4_Malaria_PK_CQCentral.mat",header=T) # from mat file
-#D4_CQ <- as.data.frame(Data4_CQ) # Convert .mat data into a data frame.
+Data4_CQ <- readMat("LateDose4_Malaria_PK_CQCentral.mat",header=T) # from mat file
+D4_CQ <- as.data.frame(Data4_CQ) # Convert .mat data into a data frame.
 
 DataMD_DQ <- readMat("data/MissedDose_Malaria_PK_DQCentral.mat",header=T) # from mat file
 DMD_DQ <- as.data.frame(DataMD_DQ) # Convert .mat data into a data frame.
@@ -129,8 +137,8 @@ Data2_DQ <- readMat("data/LateDose2_Malaria_PK_DQCentral.mat",header=T) # from m
 D2_DQ <- as.data.frame(Data2_DQ) # Convert .mat data into a data frame.
 Data3_DQ <- readMat("data/LateDose3_Malaria_PK_DQCentral.mat",header=T) # from mat file
 D3_DQ <- as.data.frame(Data3_DQ) # Convert .mat data into a data frame.
-#Data4_DQ <- readMat("LateDose4_Malaria_PK_DQCentral.mat",header=T) # from mat file
-#D4_DQ <- as.data.frame(Data4_DQ) # Convert .mat data into a data frame.
+Data4_DQ <- readMat("data/LateDose4_Malaria_PK_DQCentral.mat",header=T) # from mat file
+D4_DQ <- as.data.frame(Data4_DQ) # Convert .mat data into a data frame.
 
 
 # Define server logic required to draw a histogram
@@ -347,6 +355,70 @@ server <- function(input, output) {
     
     output$Late3DQ<- renderPlotly({ 
       pL3DQ <- ggplot(D3_DQ, aes(x = T/24, y = MedianYDQCM0)) + 
+        scale_y_continuous(limits = c(0.0, 1.2))+
+        geom_line(aes(color = 'Normal'), size = 1) +
+        geom_line(aes(y = MedianYDQCM1, color = '4.8 Hours Late'),alpha = 0.7, size = 1) +
+        geom_line(aes(y = MedianYDQCM2, color = '9.6 Hours Late'),alpha = 0.7, size = 1) +
+        geom_line(aes(y = MedianYDQCM3, color = '14.4 Hours Late'),alpha = 0.7, size = 1) +
+        geom_line(aes(y = MedianYDQCM4, color = '19.2 Hours Late'),alpha = 0.7, size = 1) +
+        geom_line(aes(y = MedianYDQCM5, color = '24 Hours Late'),alpha = 0.7, size = 1) +
+        geom_ribbon(aes(ymin=P25YDQCM0, ymax=P75YDQCM0, color = 'Normal'), fill = 'yellow', alpha=0.1) +
+        geom_ribbon(aes(ymin=P25YDQCM1, ymax=P75YDQCM1, color = '4.8 Hours Late'), fill = 'red', alpha=0.1) +
+        geom_ribbon(aes(ymin=P25YDQCM2, ymax=P75YDQCM2, color = '9.6 Hours Late'), fill = 'orange', alpha=0.1) +
+        geom_ribbon(aes(ymin=P25YDQCM3, ymax=P75YDQCM3, color = '14.4 Hours Late'), fill = 'grey', alpha=0.1) +
+        geom_ribbon(aes(ymin=P25YDQCM4, ymax=P75YDQCM4, color = '19.2 Hours Late'), fill = 'purple4', alpha=0.1) +
+        geom_ribbon(aes(ymin=P25YDQCM4, ymax=P75YDQCM5, color = '24 Hours Late'), fill = 'violetred4', alpha=0.1) +
+        
+        #formating lines
+        ggtitle('Desethylchloroquine') + # for the main title
+        xlab('Time (days)') + # for the x axis label
+        ylab('Concentration (mg/L)') + # for the y axis label
+        theme_bw()+
+        scale_color_viridis(discrete = TRUE, option = "B")+
+        theme(axis.line = element_line(colour = "black"),
+              panel.grid.major = element_blank(),
+              panel.grid.minor = element_blank(),
+              panel.border = element_blank(),
+              panel.background = element_blank(),
+              legend.title = element_blank(),
+              plot.title = element_text(size = 11, face = "bold"),
+              axis.title = element_text(size = 10))
+      
+    })
+    
+    output$Late4CQ<- renderPlotly({ 
+      pL4CQ <- ggplot(D4_CQ, aes(x = T/24, y = MedianYCQCM0)) + 
+        geom_line(aes(color = 'Normal'), size = 1) +
+        geom_line(aes(y = MedianYCQCM1, color = '4.8 Hours Late'),alpha = 0.7, size = 1) +        
+        geom_line(aes(y = MedianYCQCM2, color = '9.6 Hours Late'),alpha = 0.7, size = 1) +
+        geom_line(aes(y = MedianYCQCM3, color = '14.4 Hours Late'),alpha = 0.7, size = 1) +
+        geom_line(aes(y = MedianYCQCM4, color = '19.2 Hours Late'),alpha = 0.7, size = 1) +
+        geom_line(aes(y = MedianYCQCM5, color = '24 Hours Late'),alpha = 0.7, size = 1) +
+        geom_ribbon(aes(ymin=P25YCQCM0, ymax=P75YCQCM0, color = 'Normal'), fill = 'yellow', alpha=0.1) +
+        geom_ribbon(aes(ymin=P25YCQCM1, ymax=P75YCQCM1, color = '4.8 Hours Late'), fill = 'red', alpha=0.1) +
+        geom_ribbon(aes(ymin=P25YCQCM2, ymax=P75YCQCM2, color = '9.6 Hours Late'), fill = 'orange', alpha=0.1) +
+        geom_ribbon(aes(ymin=P25YCQCM3, ymax=P75YCQCM3, color = '14.4 Hours Late'), fill = 'grey', alpha=0.1) +
+        geom_ribbon(aes(ymin=P25YCQCM4, ymax=P75YCQCM4, color = '19.2 Hours Late'), fill = 'purple4', alpha=0.1) +
+        geom_ribbon(aes(ymin=P25YCQCM5, ymax=P75YCQCM5, color = '24 Hours Late'), fill = 'violetred4', alpha=0.1) +
+        
+        #formating lines
+        ggtitle('Chloroquine') + # for the main title
+        xlab('Time (days)') + # for the x axis label
+        ylab('Concentration (mg/L)') + # for the y axis label
+        theme_bw()+
+        scale_color_viridis(discrete = TRUE, option = "B")+
+        theme(axis.line = element_line(colour = "black"),
+              panel.grid.major = element_blank(),
+              panel.grid.minor = element_blank(),
+              panel.border = element_blank(),
+              panel.background = element_blank(),
+              legend.title = element_blank(),
+              plot.title = element_text(size = 11, face = "bold"),
+              axis.title = element_text(size = 10))
+    })
+    
+    output$Late4DQ<- renderPlotly({ 
+      pL4DQ <- ggplot(D4_DQ, aes(x = T/24, y = MedianYDQCM0)) + 
         scale_y_continuous(limits = c(0.0, 1.2))+
         geom_line(aes(color = 'Normal'), size = 1) +
         geom_line(aes(y = MedianYDQCM1, color = '4.8 Hours Late'),alpha = 0.7, size = 1) +
